102a649f276f781cdaa49d767782f81dcbd68356
CarloP95 <carlop95@hotmail.it>
2019-08-07 11:24:00 +0200

    Test to fix callback problem

--- src/dataManager/DataManager.js
+++ src/dataManager/DataManager.js
@@ -59,18 +59,21 @@ class AdcBrightnessReader {
 	}
 
 	read() {
-
-		return this.adc.readChannel(ADS1x15.channel.CHANNEL_0, (err, value, volts) => {
+		
+		let val, volt = val = 0;
+		
+		this.adc.readChannel(ADS1x15.channel.CHANNEL_0, (err, value, volts) => {
 			if (err)
 				console.error('ADC is giving problems.. ', err);
 
 			else {
 				console.log(`Channel 0 -- Value: ${value} - Volts: ${volts}`);
+				val = value; volt = volts;
 			}
 
-			return [value, volts];
-
 		});
+		
+		return [val, volt];
 
 	}
 
@@ -254,25 +257,6 @@ function endRoutine() {
 
 process.on('SIGINT', endRoutine);
 
-const i2c = new I2C();
-
-const adc = new ADS1x15({
-	i2c,
-	chip: 		ADS1x15.chips.IC_ADS1115,
-	address:	ADS1x15.address.ADDRESS_0x48,
-
-	pga: 		ADS1x15.pga.PGA_4_096V,
-	sps:		ADS1x15.spsADS1115.SPS_250
-});
-
-adc.readChannel(ADS1x15.channel.CHANNEL_0, (err, value, volts) => {
-	if (err)
-		console.error('ADC is giving problems.. ', err);
-
-	else {
-		console.log(`Channel 0 -- Value: ${value} - Volts: ${volts}`);
-	}
-});
 
 module.exports = {
 	EnvironmentalInput,
